84825b9017f550b66b54b5f572e4c16b288eb481
unknown <keresztesvitez@gmail.com>
2017-05-29 21:40:30 +0200

    refactored

--- src/main/java/com/epam/training/Role.java
+++ src/main/java/com/epam/training/Role.java
@@ -1,5 +1,8 @@
 package com.epam.training;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public enum Role {
 	
 	ADMIN(true, 10), USER(true, 8), TRAINER(true, 9), PARTICIPANT(false, 2, false), DEVELOPER(true, 5), ASSISTANT(false, 4);
@@ -36,6 +39,48 @@ public enum Role {
 
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
+	}
+
+	public static Set<Role> getElevatedRoles() {
+		Set<Role> roles = new HashSet<Role>();
+		for (Role currentRole : Role.values()) {
+			if (currentRole.isEnabled() && currentRole.isElevated()) {
+				roles.add(currentRole);
+			}
+		}
+		return roles;
+	}
+
+	public static Set<Role> getRolesAboveAccessLevel(int accessLevel) {
+		Set<Role> roles = new HashSet<Role>();
+		for (Role currentRole : Role.values()) {
+			if (currentRole.getAccessLevel() >= accessLevel) {
+				if (currentRole.isEnabled()) {
+					roles.add(currentRole);
+				}
+			}
+		}
+		return roles;
+	}
+
+	public static Set<Role> getRolesBelowAccessLevel(int accessLevel) {
+		Set<Role> roles = new HashSet<Role>();
+		for (Role currentRole : Role.values()) {
+			if (currentRole.getAccessLevel() < accessLevel && currentRole.isEnabled()) {
+				roles.add(currentRole);
+			}
+		}
+		return roles;
+	}
+
+	public static Set<Role> getElevatedRolesOnAccessLevel(int accessLevel) {
+		Set<Role> roles = new HashSet<Role>();
+		for (Role currentRole : Role.values()) {
+			if (currentRole.getAccessLevel() == accessLevel && currentRole.isEnabled() && currentRole.isElevated()) {
+				roles.add(currentRole);
+			}
+		}
+		return roles;
 	}
 
 }
